0327feb49a64b15a22500384f932451f30394703
gcurtil <75056373+gcurtil@users.noreply.github.com>
2025-08-09 20:16:36 +0000

    some changes, create output dirs, disable py cxx version as it is hanging from CodeSpaces

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -2,6 +2,12 @@
 
 cd /app
 
+sudo chmod 777 /app/output/
+mkdir -p /app/output/dnsperfcxx
+mkdir -p /app/output/dnsperfgo
+mkdir -p /app/output/dnsperfpy
+mkdir -p /app/output/dnsperfpycxx
+
 # Run DNS perf queries
 # for i in {1..5}; do 
 n=5
@@ -20,22 +26,15 @@ for ((i=0; i < n; i++)); do
     time python3 /app/python/pydnsperf_main.py --servers servers.txt --domains domains.txt --output /app/output/dnsperfpy 
     sleep 1
 
-    echo "Running Python with C++ wrapper version"
-    time python3 /app/python/pydnsperf_main.py --servers servers.txt --domains domains.txt --output /app/output/dnsperfpycxx --resolve-impl cxx
-    sleep 1
+    # echo "Running Python with C++ wrapper version"
+    # time python3 /app/python/pydnsperf_main.py --servers servers.txt --domains domains.txt --output /app/output/dnsperfpycxx --resolve-impl cxx
+    # sleep 1
 done
 
 # sync data to SQL db
 time /app/golang/dbsync --ldbpath /app/output/dnsperfgo --sqldbpath /app/output/dnsgo.db --verbose true 
 time /app/golang/dbsync --ldbpath /app/output/dnsperfcxx --sqldbpath /app/output/dnscxx.db --verbose true 
 time /app/golang/dbsync --ldbpath /app/output/dnsperfpy --sqldbpath /app/output/dnspy.db --verbose true 
-time /app/golang/dbsync --ldbpath /app/output/dnsperfpycxx --sqldbpath /app/output/dnspycxx.db --verbose true 
-
-
-
-
-
-
-
+# time /app/golang/dbsync --ldbpath /app/output/dnsperfpycxx --sqldbpath /app/output/dnspycxx.db --verbose true 
 
 
